27d43280765ee164cdda4c8c9390edd93571a66a
benngo89 <ben_ngo89@yahoo.com>
2019-04-16 14:23:33 -0400

    Finish product

---
+++ key.js
@@ -0,0 +1,11 @@
+exports.twitterKeys = {
+    consumer_key: 'm4Ot0Ddav7OPRu2TOQBZI5O7H',
+    consumer_secret: 'rBymtHEc0hP12G2gI9op34XR7LPpZ0WYucNpft5wGjRtbEE2z3',
+    access_token_key: '1018569715302715392-0o9MQ8HgUiqw5h0ZBA2RtG4bBkyWxw',
+    access_token_secret: 'W4Qq2MTWsn6d7rLaaxM8zru9RGTifrNredjeBVZFFdtFl',
+}
+
+exports.spotifyKeys = {
+    id: '5cd1d172f98d42cd9296df65e920a800',
+    secret: '13d2cf2f85d04119bdebc354afceef57',
+}
